9e3f27a7b2e2dcf603317d1158f51abf6ecc4561
joaoparracho <35959030+joaoparracho@users.noreply.github.com>
2019-12-26 16:37:02 +0000

    nao mexas Anastacio, vai ver o meu workshop de git :)

--- otimizedMachineLearning.py
+++ otimizedMachineLearning.py
@@ -3,17 +3,47 @@ from functionOtimize import *
 import argparse
 clusteringMtd=[clusterAlgorithm,kMeansAlgorithm]
 
-#datasetpath=sys.argv[1]
-datasetpath="D:\\IPL\\Mestrado\\1_Ano\\OML\\Projecto\\Clustering_MachineLearning\\Datasets_GroupG.xlsx"
-datasetUnsupervised=pd.read_excel(datasetpath,skiprows=1)
-labelsUnsupervised=datasetUnsupervised.columns.values
-print(labelsUnsupervised)
-print(datasetUnsupervised)
-#print(dataset.values)
-#data=dataset.values[1:,:]
-#print(dataset.isnull().sum())
-#print(dataset)
-#dataset.fillna(dataset.mean(), inplace=True)
-# count the number of NaN values in each column
-#print(dataset.isnull().sum())
-# %%
+def readArgs():
+    parser = argparse.ArgumentParser(description='Optimization and Machine Learning (MEEE-IPL).'
+                                    ,formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-d',type=str,
+    help='''Dataset Information: 
+    1 - Dataset Path (.xlsx)
+    2 - Number of rows to skip in dataset file 
+    3 - Sheet Name''')
+    parser.add_argument('-c',type=int,
+    help='''Compute missing data: 
+    No  : 0 
+    Yes : 1''')
+    parser.add_argument('-a',type=int,
+    help='''Adapt Data:
+    Normalize    : 0 
+    Standardize  : 1''')
+    parser.add_argument('-dt',type=str,
+    help='''Pairwise Distance method availabe: braycurtis, canberra, chebyshev,
+    cityblock, correlation, cosine, dice, euclidean, hamming, jaccard,
+    jensenshannon, kulsinski, mahalanobis, matching, minkowski, rogerstanimoto, 
+    russellrao, seuclidean, sokalmichener, sokalsneath, sqeuclidean, yule''')
+    parser.add_argument('-ml',type=str,
+    help="Linkage method available: single, complete, average, weighted, centeroid, median, ward")
+    parser.add_argument('-nc',type=int, help="Number of Clusters")
+    parser.add_argument('-cmt',type=int, help='''Select Clustering Method: 
+    Cluster: 0
+    K-Means: 1''')
+
+    args = parser.parse_args()
+    return args.d.split(" "),args.c,args.a,args.dt,args.ml,args.nc,args.cmt
+
+[[datasetpath,numSkipedRow,sheetname],cmpMissData,adaptData,distanceMethod,linkageMethod,numCluster,cmt]=readArgs()
+
+dataset=readExcel(datasetpath,int(numSkipedRow),sheetname)
+[labels,data,dataDist,dataLink]=computeExcelData(dataset,cmpMissData,adaptData,distanceMethod,linkageMethod)
+clusteringMtd[cmt](data,dataLink,numCluster)
+
+plotBiDispersidade(data,None,0)
+plotFunction(dendrogram,dataLink,labels=np.array(dataset.values)[:,0],title='Denogram', ylabel='Distance',xlabel='ID')
+
+
+plt.show()
+# %%prin
+
