99f6aa37e60f3839290d5c1aeb62f2b57edf0545
Ricardo Anastácio <ricardoanastacio123@gmail.com>
2019-12-29 19:09:35 +0000

    last commit

--- functionOtimize.py
+++ functionOtimize.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist,squareform #funções pdist e square form deve ser obtidas a partir do package scipy.spatial.distance
 from scipy.cluster.hierarchy import dendrogram, linkage,fcluster
